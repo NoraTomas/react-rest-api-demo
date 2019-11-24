7025719022b959725a485dc3ebeac471472a92a8
Nora Tomas <noratomas@Noras-MacBook-Pro.local>
2019-11-24 11:27:48 +0100

    more stuff

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,46 +1,129 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Box } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-
+import TextField from "@material-ui/core/TextField";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+const devlinStyle = makeStyles(theme =>
+  createStyles({
+    button: {
+      margin: theme.spacing(1)
+    }
+  })
+);
 export default function MemberList() {
+  const { button } = devlinStyle();
+
   const [members, setMembers] = useState([]);
+  const [member, setMember] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadMembers();
   }, []);
 
   const loadMembers = async () => {
+    setLoading(true);
     const response = await axios.get("http://localhost:5000/members");
-    console.log("RESPONSE DATA", response.data);
+    console.log("RESPONSE_DATA: ", response.data);
     setMembers(response.data);
+    setLoading(false);
   };
 
   const deleteMember = async id => {
-    await axios.delete("http://localhost:5000/members/" + id);
-    const newMembers = members.filter(m => m.id != id);
+    const previous = [...members];
+    const newMembers = members.filter(m => m.id !== id);
     setMembers(newMembers);
+
+    try {
+      await axios.delete(`http://localhost:5000/members/${id}`);
+    } catch (err) {
+      alert(err.message);
+      setMembers(previous);
+    }
+  };
+
+  const handleAddMember = async () => {
+    setLoading(true);
+    try {
+      const { data: createdMember } = await axios.post(
+        "http://localhost:5000/members",
+        member
+      );
+      const newMemberCollection = [...members, createdMember];
+      setMembers(newMemberCollection);
+    } catch (e) {
+      alert(e.message);
+    }
+    setLoading(false);
+  };
+
+  const handleNameOnChange = ({ currentTarget }) => {
+    console.log(currentTarget.value);
+    setMember({ ...member, name: currentTarget.value });
+  };
+
+  const handleAgeOnChange = ({ currentTarget }) => {
+    console.log(currentTarget.value);
+    setMember({ ...member, age: currentTarget.value });
   };
 
   return (
-    <div>
+    <div style={{ width: "75vw", margin: "0 auto", padding: "2rem" }}>
       <h2>React Demo</h2>
       <h3>
-        <ul>
-          {members.map(m => (
-            <Box boxShadow={3}>
-              <ul key={m.id}>
-                {m.name} {m.age}
-              </ul>
-              <Button
-                onClick={() => deleteMember(m.id)}
-                variant="contanied"
-                color="secondary"
-              >
-                Delete
-              </Button>
-            </Box>
-          ))}
+        <TextField
+          onChange={handleNameOnChange}
+          margin="dense"
+          variant="outlined"
+          multiline
+          rowsMax="4"
+        />
+        <TextField
+          onChange={handleAgeOnChange}
+          margin="dense"
+          variant="outlined"
+          multiline
+          rowsMax="4"
+        />
+        <Button variant="contained" color="primary" onClick={handleAddMember}>
+          Add Member
+        </Button>
+        <ul style={{ listStyle: "none" }}>
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : (
+            members.map(m => (
+              <Box key={m.id} boxShadow={3}>
+                <li style={{ margin: "1rem" }}>
+                  {m.name} {m.age}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: "1rem" }}
+                    onClick={() => alert("Edit")}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    variant="contained"
+                    className={button}
+                    onClick={() => alert("Update")}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={{ margin: "1rem" }}
+                    onClick={() => deleteMember(m.id)}
+                  >
+                    Delete
+                  </Button>
+                </li>
+              </Box>
+            ))
+          )}
         </ul>
       </h3>
     </div>
